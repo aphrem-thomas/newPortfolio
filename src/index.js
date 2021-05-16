f2eb36460f5ea7b6e43a0ee134e690d3f190bb1b
aphrem-thomas <aphremthomas@gmail.com>
2021-05-17 00:08:49 +0530

    setup done

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+  return (
+    <div>
+        <div>Howdi Partner...</div>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.querySelector("#app"));
